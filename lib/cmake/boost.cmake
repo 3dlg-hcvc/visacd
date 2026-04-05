c853b89e9747785e85933dbc2aa6d24fcdfcef3f
EgorFokin <egor.fokin2017@gmail.com>
2026-04-04 17:11:06 -0700

    make boost.cmake support old cmake

--- lib/cmake/boost.cmake
+++ lib/cmake/boost.cmake
@@ -5,19 +5,25 @@ set(BOOST_IOSTREAMS_ENABLE_BZIP2 OFF)
 
 include(FetchContent)
 
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.24")
+    set(_BOOST_OVERRIDE OVERRIDE_FIND_PACKAGE)
+else()
+    set(_BOOST_OVERRIDE "")
+endif()
+
 if(WIN32)
     FetchContent_Declare(
         boost
         URL https://github.com/boostorg/boost/releases/download/boost-1.81.0/boost-1.81.0.zip
-        URL_HASH MD5=375693214b89309d2003f5296422c0a8
-        OVERRIDE_FIND_PACKAGE
+        URL_HASH SHA256=ef6cc4ffc95703dfa7e9d21789b27d57c666bcbeced9f6f49162382bb2ea343f
+        ${_BOOST_OVERRIDE}
     )
 else()
     FetchContent_Declare(
         boost
         URL https://github.com/boostorg/boost/releases/download/boost-1.81.0/boost-1.81.0.tar.gz
-        URL_HASH MD5=ffac94fbdd92d6bc70a897052022eeba
-        OVERRIDE_FIND_PACKAGE
+        URL_HASH SHA256=121da556b718fd7bd700b5f2e734f8004f1cfa78b7d30145471c526ba75a151c
+        ${_BOOST_OVERRIDE}
     )
 endif()
 
